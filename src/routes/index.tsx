959c4a3b47f4d4c6ce03377fab8e7e505b2ce4bf
Henrique Pantarotto <henrique@henpa.com>
2023-12-22 16:30:33 -0300

    exemplo1

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,23 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { server$, type DocumentHead } from "@builder.io/qwik-city";
+
+const servertest = server$((text: string) => {
+  console.log(text);
+  return "hello from server";
+});
 
 export default component$(() => {
   return (
     <>
       <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <button
+        onClick$={async () => {
+          const greeting = await servertest("hello from client");
+          console.log(greeting);
+        }}
+      >
+        greet
+      </button>
     </>
   );
 });
